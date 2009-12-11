5872e2ca8cc5093bd582df587c138b19408a3f63
Johannes Schlüter <johannes@schlueters.de>
2009-12-11 19:04:06 +0100

    kick ext_skel rubbish

--- basic.c
+++ basic.c
@@ -27,20 +27,26 @@
 #include "ext/standard/info.h"
 #include "php_basic.h"
 
-/* If you declare any globals in php_basic.h uncomment this:
-ZEND_DECLARE_MODULE_GLOBALS(basic)
-*/
+/* {{{ proto string basic_compile(string function_name, string filename)
+   compile filename into function function_name */
+PHP_FUNCTION(basic_compile)
+{
+	char *arg = NULL;
+	int arg_len, len;
+	char *strg;
 
-/* True global resources - no need for thread safety here */
-static int le_basic;
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
+		return;
+	}
+
+	RETURN_TRUE;
+}
 
 /* {{{ basic_functions[]
- *
- * Every user visible function must have an entry in basic_functions[].
  */
 const zend_function_entry basic_functions[] = {
-	PHP_FE(confirm_basic_compiled,	NULL)		/* For testing, remove later. */
-	{NULL, NULL, NULL}	/* Must be the last line in basic_functions[] */
+	PHP_FE(basic_compile,	NULL)
+	{NULL, NULL, NULL}
 };
 /* }}} */
 
@@ -52,13 +58,13 @@ zend_module_entry basic_module_entry = {
 #endif
 	"basic",
 	basic_functions,
-	PHP_MINIT(basic),
-	PHP_MSHUTDOWN(basic),
-	PHP_RINIT(basic),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(basic),	/* Replace with NULL if there's nothing to do at request end */
+	NULL,
+	NULL,
+	NULL,
+	NULL,
 	PHP_MINFO(basic),
 #if ZEND_MODULE_API_NO >= 20010901
-	"0.1", /* Replace with version number for your extension */
+	"0.1",
 #endif
 	STANDARD_MODULE_PROPERTIES
 };
@@ -78,57 +84,6 @@ PHP_INI_END()
 */
 /* }}} */
 
-/* {{{ php_basic_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_basic_init_globals(zend_basic_globals *basic_globals)
-{
-	basic_globals->global_value = 0;
-	basic_globals->global_string = NULL;
-}
-*/
-/* }}} */
-
-/* {{{ PHP_MINIT_FUNCTION
- */
-PHP_MINIT_FUNCTION(basic)
-{
-	/* If you have INI entries, uncomment these lines 
-	REGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_MSHUTDOWN_FUNCTION
- */
-PHP_MSHUTDOWN_FUNCTION(basic)
-{
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
-}
-/* }}} */
-
-/* Remove if there's nothing to do at request start */
-/* {{{ PHP_RINIT_FUNCTION
- */
-PHP_RINIT_FUNCTION(basic)
-{
-	return SUCCESS;
-}
-/* }}} */
-
-/* Remove if there's nothing to do at request end */
-/* {{{ PHP_RSHUTDOWN_FUNCTION
- */
-PHP_RSHUTDOWN_FUNCTION(basic)
-{
-	return SUCCESS;
-}
-/* }}} */
-
 /* {{{ PHP_MINFO_FUNCTION
  */
 PHP_MINFO_FUNCTION(basic)
@@ -142,35 +97,6 @@ PHP_MINFO_FUNCTION(basic)
 	*/
 }
 /* }}} */
-
-
-/* Remove the following function when you have succesfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_basic_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_basic_compiled)
-{
-	char *arg = NULL;
-	int arg_len, len;
-	char *strg;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
-
-	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "basic", arg);
-	RETURN_STRINGL(strg, len, 0);
-}
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and 
-   unfold functions in source code. See the corresponding marks just before 
-   function definition, where the functions purpose is also documented. Please 
-   follow this convention for the convenience of others editing your code.
-*/
-
 
 /*
  * Local variables:
--- php_basic.h
+++ php_basic.h
@@ -32,46 +32,7 @@ extern zend_module_entry basic_module_entry;
 #	define PHP_BASIC_API
 #endif
 
-#ifdef ZTS
-#include "TSRM.h"
-#endif
-
-PHP_MINIT_FUNCTION(basic);
-PHP_MSHUTDOWN_FUNCTION(basic);
-PHP_RINIT_FUNCTION(basic);
-PHP_RSHUTDOWN_FUNCTION(basic);
-PHP_MINFO_FUNCTION(basic);
-
-PHP_FUNCTION(confirm_basic_compiled);	/* For testing, remove later. */
-
-/* 
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:     
-
-ZEND_BEGIN_MODULE_GLOBALS(basic)
-	long  global_value;
-	char *global_string;
-ZEND_END_MODULE_GLOBALS(basic)
-*/
-
-/* In every utility function you add that needs to use variables 
-   in php_basic_globals, call TSRMLS_FETCH(); after declaring other 
-   variables used by that function, or better yet, pass in TSRMLS_CC
-   after the last function argument and declare your utility function
-   with TSRMLS_DC after the last declared argument.  Always refer to
-   the globals in your function as BASIC_G(variable).  You are 
-   encouraged to rename these macros something shorter, see
-   examples in any other php module directory.
-*/
-
-#ifdef ZTS
-#define BASIC_G(v) TSRMG(basic_globals_id, zend_basic_globals *, v)
-#else
-#define BASIC_G(v) (basic_globals.v)
-#endif
-
 #endif	/* PHP_BASIC_H */
-
 
 /*
  * Local variables:
